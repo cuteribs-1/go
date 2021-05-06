307ae7964b4808eb0df9ed1a06ebf412d96360a2
Cuteribs <ericfine1981@live.com>
2021-05-06 15:10:22 +0800

    1

---
+++ shimo/readme.md
@@ -0,0 +1,5 @@
+### build
+```
+go build -ldflags "-s" shimo.go
+upx shimo.exe
+```
--- shimo/shimo.go
+++ shimo/shimo.go
@@ -14,17 +14,27 @@ import (
 func main(){
 	fmt.Println("------------------------------")
 	fmt.Println("排骨的石墨 MarkDown 图片下载工具")
-	fmt.Println("shimo.exe [sample.md] [accessToken]")
+	fmt.Println("shimo.exe [accessToken]")
 	fmt.Println("------------------------------")
 
-	if len(os.Args) < 3 { 
-		fmt.Println("lack of parameters")
+	if len(os.Args) < 2 { 
+		fmt.Println("缺少参数")
 		return
 	}
 
-	mdFilePath := os.Args[1]
-	token := os.Args[2]
+	currentFolder := filepath.Dir(os.Args[0])
+	token := os.Args[1]
+	files, _ := ioutil.ReadDir(currentFolder)
 
+	for _, file := range files {
+		if filepath.Ext(file.Name()) != ".md" { continue }
+		mdFilePath := filepath.Join(currentFolder, file.Name())
+		fmt.Println("读取 ->", mdFilePath)
+		DealWithMDFile(mdFilePath, token)
+	}	
+}
+
+func DealWithMDFile(mdFilePath string, token string){
 	folder := strings.TrimRight(mdFilePath, ".md")
 	os.MkdirAll(folder, os.ModePerm)
 	content, _ := ioutil.ReadFile(mdFilePath)
@@ -33,17 +43,28 @@ func main(){
 	re := regexp.MustCompile(`!\[(.*)\]\((https://uploader.shimo.im/.*)\)`)
 	matches := re.FindAllStringSubmatch(md, -1)
 
+	if len(matches) == 0 {
+		fmt.Println("忽略 ->", mdFilePath)
+		return
+	}
+
 	for _, match := range matches {
 		originUrl := match[2]
-		url := originUrl[0:strings.Index(originUrl, "!thumbnail")]
+		url := originUrl[0:strings.Index(originUrl, "?fileGuid")]
+		
+		if strings.Contains(originUrl, "!thumbnail") {
+			url = originUrl[0:strings.Index(originUrl, "!thumbnail")]
+		}
+
 		fileName := url[strings.LastIndex(url, "/") + 1:]
-		url = url + "?accessToken=" + token
-		fmt.Println("pulling ", fileName)
-	  Download(url, filepath.Join(folder, fileName))
+		targetUrl := url + "?accessToken=" + token
+		fmt.Println("下载 ->", url)
+	  Download(targetUrl, filepath.Join(folder, fileName))
 		relativePath := "./" + filepath.Base(folder) + "/" + fileName
 		md = strings.Replace(md, originUrl, relativePath, -1)
 	}
 
+	fmt.Println("修改 ->", mdFilePath)
 	file, _ := os.OpenFile(mdFilePath, os.O_RDWR, 0755)
   defer file.Close()
   file.WriteString(md)
